ce8ed139b6beb6b3446a6fa236f931c8e528fad2
fathi <fathi.murad001@stud.fh-dortmund.de>
2022-05-23 11:51:40 +0200

    Klausur

--- EinPAlteKlausuren/src/Klausur_27_08_2021/Regal.java
+++ EinPAlteKlausuren/src/Klausur_27_08_2021/Regal.java
@@ -3,13 +3,26 @@ package Klausur_27_08_2021;
 public class Regal {
 
 	// AssozioationsAttribute
-	private RegalBrett[] regalBrett = new ReaglBrett[10]; // 0..9
+	private RegalBrett[] regalBrett = new RegalBrett[10]; // 0..9
 	private int anzahlRegalBrett;
 	
 	public Regal( RegalBrett regalBrett1, RegalBrett regalBrett2 ) {
 		regalBrett[0] = regalBrett1;
 		regalBrett[1] = regalBrett2;
 		anzahlRegalBrett = 2;
+	}
+	
+	public boolean fuegeHinzu( Medium medium ) {
+		boolean erg = false;
+		
+		for(int i = 0; i < anzahlRegalBrett; i++) {
+			if(regalBrett[i].addMedium(medium) == true) {
+				erg = true;
+				break;
+			}
+		}
+		
+		return erg;
 	}
 	
 }
--- EinPAlteKlausuren/src/Klausur_27_08_2021/RegalBrett.java
+++ EinPAlteKlausuren/src/Klausur_27_08_2021/RegalBrett.java
@@ -5,7 +5,7 @@ public class RegalBrett {
 	private int breite;
 	private double tragkraft;
 	
-	// AssoziazionsAttribute
+	// AssoziationsAttribute
 	private Medium[] medien = new Medium[100];
 	private int anzahlMedien;
 	
@@ -16,17 +16,39 @@ public class RegalBrett {
 	
 	// AssoziationsMethoden (add, get, remove)
 	public boolean addMedium(Medium medium) {
-		
 		boolean result = false;
-		
 		if( anzahlMedien < 100 ) {
-			
-			
-			
+			int summeBreite = 0;
+			int summeGewicht = 0;
+			for(int i = 0; i < anzahlMedien; i++) {
+				summeBreite += medien[i].getBreite();
+				summeGewicht += medien[i].getGewicht();
+			}
+			if( summeBreite+medium.getBreite() <= this.breite
+					&& summeGewicht+medium.getGewicht() <= this.tragkraft ) {
+				medien[anzahlMedien] = medium;
+				anzahlMedien++;
+				this.breite -= medium.getBreite();
+				this.tragkraft -= medium.getGewicht();
+				result = true;
+			}
 		}
-		
 		return result;
-		
+	}
+	
+	public Medium getMedium(int pos) {
+		if(pos >= 0 && pos < medien.length) {
+			return medien[pos];
+		} else {
+			return null;
+		}
+	}
+	
+	public void removeMedium(int pos) {
+		if(pos >=0 && pos < medien.length) {
+			medien[pos] = null;
+			anzahlMedien--;
+		}
 	}
 	
 }
